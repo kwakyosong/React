3d14be196c7c4bb990376605ed6bd92f4d0288b1
kwakyosong <kwakyosong.hk@gmail.com>
2023-02-15 16:24:15 +0900

    초기소스

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom'
+import Home from './Home';
+import About from './About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>테스트</h1>
+      <div>
+        <Route path="/" component={Home} exact={true}/>
+        <Route path="/about" component={About} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
